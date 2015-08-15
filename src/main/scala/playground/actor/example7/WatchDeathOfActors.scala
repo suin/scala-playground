31df150afd066ac4e36dce053678c3782361d8fa
Hidehito Nozawa <suinyeze@gmail.com>
2015-08-15 22:05:44 +0900

    Add example log for WatchDeathOfActors.
    Signed-off-by: Hidehito Nozawa <suinyeze@gmail.com>

--- src/main/scala/playground/actor/example7/WatchDeathOfActors.scala
+++ src/main/scala/playground/actor/example7/WatchDeathOfActors.scala
@@ -50,3 +50,16 @@ object WatchDeathOfActors extends App {
   Thread.sleep(500L)
   system.shutdown()
 }
+
+// 出力されるログの例
+// [INFO] [08/15/2015 22:05:06.228] [death-of-child-actor-akka.actor.default-dispatcher-3] [akka://death-of-child-actor/user/parent] parent is starting
+// [INFO] [08/15/2015 22:05:06.230] [death-of-child-actor-akka.actor.default-dispatcher-2] [akka://death-of-child-actor/user/parent/child1] child1 is starting
+// [INFO] [08/15/2015 22:05:06.230] [death-of-child-actor-akka.actor.default-dispatcher-5] [akka://death-of-child-actor/user/parent/child2] child2 is starting
+// [INFO] [08/15/2015 22:05:06.230] [death-of-child-actor-akka.actor.default-dispatcher-6] [akka://death-of-child-actor/user/parent/child3] child3 is starting
+// [INFO] [08/15/2015 22:05:06.233] [death-of-child-actor-akka.actor.default-dispatcher-6] [akka://death-of-child-actor/user/parent/child3] child3 has been stopped
+// [INFO] [08/15/2015 22:05:06.233] [death-of-child-actor-akka.actor.default-dispatcher-2] [akka://death-of-child-actor/user/parent/child1] child1 has been stopped
+// [INFO] [08/15/2015 22:05:06.233] [death-of-child-actor-akka.actor.default-dispatcher-5] [akka://death-of-child-actor/user/parent/child2] child2 has been stopped
+// [INFO] [08/15/2015 22:05:06.237] [death-of-child-actor-akka.actor.default-dispatcher-3] [akka://death-of-child-actor/user/parent] child is dead: akka://death-of-child-actor/user/parent/child3. Now children total are 2.
+// [INFO] [08/15/2015 22:05:06.238] [death-of-child-actor-akka.actor.default-dispatcher-3] [akka://death-of-child-actor/user/parent] child is dead: akka://death-of-child-actor/user/parent/child2. Now children total are 1.
+// [INFO] [08/15/2015 22:05:06.238] [death-of-child-actor-akka.actor.default-dispatcher-3] [akka://death-of-child-actor/user/parent] child is dead: akka://death-of-child-actor/user/parent/child1. Now children total are 0.
+// [INFO] [08/15/2015 22:05:06.731] [death-of-child-actor-akka.actor.default-dispatcher-3] [akka://death-of-child-actor/user/parent] parent has been stopped
