d58b52f94e3912742d44b7fbac9df76a2c015020
Hidehito Nozawa <suinyeze@gmail.com>
2016-02-16 09:45:32 +0900

    Add an example using Option[xml.Text] to create XML element with an optional attribute
    Signed-off-by: Hidehito Nozawa <suinyeze@gmail.com>

--- src/main/scala/playground/xml/XmlElementWithAnOptionalAttribute.scala
+++ src/main/scala/playground/xml/XmlElementWithAnOptionalAttribute.scala
@@ -2,14 +2,24 @@
 
 package playground.xml
 
-object XmlElementWithAnOptionalAttribute extends App {
-  def getElem(foo: Option[String]) = <elem foo={ foo.orNull }></elem>
+import scala.xml.Text
 
+object XmlElementWithAnOptionalAttribute extends App {
+  // orElseを使う方法
+  def getElem1(foo: Option[String] = None) = <elem foo={ foo.orNull }></elem>
+
+  // nullを使う方法
   def getElem2(foo: String = null) = <elem foo={ foo }></elem>
 
-  assert(getElem(None) == <elem></elem>)
-  assert(getElem(Some("bar")) == <elem foo="bar"></elem>)
+  // Option[xml.Text]を使う方法
+  def getElem3(foo: Option[Text] = None) = <elem foo={ foo }></elem>
+
+  assert(getElem1() == <elem></elem>)
+  assert(getElem1(Some("bar")) == <elem foo="bar"></elem>)
 
   assert(getElem2() == <elem></elem>)
   assert(getElem2("bar") == <elem foo="bar"></elem>)
+
+  assert(getElem3() == <elem></elem>)
+  assert(getElem3(Some(Text("bar"))) == <elem foo="bar"></elem>)
 }
