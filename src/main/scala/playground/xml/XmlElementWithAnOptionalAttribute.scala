e0809881779fd3f2e4a619944f810988eee099ad
Hidehito Nozawa <suinyeze@gmail.com>
2016-02-16 09:05:20 +0900

    Add another example of creating XML element with an optional attribute
    Signed-off-by: Hidehito Nozawa <suinyeze@gmail.com>

--- src/main/scala/playground/xml/XmlElementWithAnOptionalAttribute.scala
+++ src/main/scala/playground/xml/XmlElementWithAnOptionalAttribute.scala
@@ -5,6 +5,11 @@ package playground.xml
 object XmlElementWithAnOptionalAttribute extends App {
   def getElem(foo: Option[String]) = <elem foo={ foo.orNull }></elem>
 
+  def getElem2(foo: String = null) = <elem foo={ foo }></elem>
+
   assert(getElem(None) == <elem></elem>)
   assert(getElem(Some("bar")) == <elem foo="bar"></elem>)
+
+  assert(getElem2() == <elem></elem>)
+  assert(getElem2("bar") == <elem foo="bar"></elem>)
 }
